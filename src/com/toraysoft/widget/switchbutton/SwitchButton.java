d1ccad1428462518b3db3597a193c4affb1e3321
jclang <liang@toraysoft.com>
2014-08-13 10:55:09 +0800

    add SwitchButton

---
+++ src/com/toraysoft/widget/switchbutton/SwitchButton.java
@@ -0,0 +1,238 @@
+package com.toraysoft.widget.switchbutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.RelativeLayout;
+
+public class SwitchButton extends RelativeLayout implements OnGestureListener {
+
+	GestureDetector mDetector;
+
+	ImageView bg_ImageView, tag_ImageView;
+	LayoutParams mLayoutParams;
+
+	FlingRunnable mFlinger;
+
+	int width;
+	int height;
+	int sliderWidth;
+	int sliderHeight;
+	int maxSliderWidth;
+	int minSliderWidth;
+	int bg_imgId;
+	int on_imgId;
+	int off_imgId;
+
+	float xLast = 0;
+
+	public enum Slider {
+		LEFT, RIGHT,
+	}
+
+	Slider mSlider = Slider.LEFT;
+	OnSliderChangeListener mOnSliderChangeListener;
+
+	public SwitchButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public SwitchButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public SwitchButton(Context context) {
+		super(context);
+	}
+
+	public void init(int width, int height, int sliderWidth, int sliderHeight,
+			int bgImgId, int onImgId, int offImgId) {
+		this.width = width;
+		this.height = height;
+		this.sliderWidth = sliderWidth;
+		this.sliderHeight = sliderHeight;
+		this.maxSliderWidth = width - sliderWidth;
+		this.minSliderWidth = width - sliderWidth / 2;
+		this.bg_imgId = bgImgId;
+		this.on_imgId = onImgId;
+		this.off_imgId = offImgId;
+		setLayoutParams(new LayoutParams(width, height));
+
+		bg_ImageView = new ImageView(getContext());
+		bg_ImageView.setLayoutParams(new LayoutParams(width, height));
+		bg_ImageView.setScaleType(ScaleType.FIT_XY);
+		bg_ImageView.setImageResource(this.bg_imgId);
+		bg_ImageView.setClickable(false);
+		this.addView(bg_ImageView);
+
+		tag_ImageView = new ImageView(getContext());
+		tag_ImageView.setScaleType(ScaleType.FIT_XY);
+		mLayoutParams = new LayoutParams(this.sliderWidth, this.sliderHeight);
+		tag_ImageView.setLayoutParams(mLayoutParams);
+		tag_ImageView.setImageResource(this.off_imgId);
+		tag_ImageView.setClickable(false);
+		this.addView(tag_ImageView);
+
+		mDetector = new GestureDetector(getContext(), this);
+		mDetector.setIsLongpressEnabled(false);
+		mFlinger = new FlingRunnable();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			xLast = (int) event.getX();
+			move(xLast);
+			break;
+		case MotionEvent.ACTION_UP:
+			xLast = event.getX();
+			int des = (int) xLast - sliderWidth / 2;
+			if (des < 0)
+				des = 0;
+			if (des > maxSliderWidth)
+				des = maxSliderWidth;
+			boolean isMoveLeft = xLast < width / 2;
+			mFlinger.setMoveLeft(isMoveLeft);
+			mFlinger.startUsingDistance(xLast);
+			done(isMoveLeft);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			xLast = (int) event.getX();
+			move(xLast);
+			break;
+		}
+		return true;
+	}
+
+	private void move(float x) {
+		int des = (int) x - sliderWidth / 2;
+		if (des < 0)
+			des = 0;
+		if (des > maxSliderWidth)
+			des = maxSliderWidth;
+		mLayoutParams.setMargins(des, 0, 0, 0);
+		tag_ImageView.setLayoutParams(mLayoutParams);
+	}
+
+	private void done(boolean isMoveLeft) {
+		if (isMoveLeft && mSlider != Slider.LEFT) {
+			mSlider = Slider.LEFT;
+			tag_ImageView.setImageResource(off_imgId);
+			if (mOnSliderChangeListener != null) {
+				mOnSliderChangeListener.onChange(mSlider);
+			}
+			return;
+		}
+		if (!isMoveLeft && mSlider != Slider.RIGHT) {
+			mSlider = Slider.RIGHT;
+			tag_ImageView.setImageResource(on_imgId);
+			if (mOnSliderChangeListener != null) {
+				mOnSliderChangeListener.onChange(mSlider);
+			}
+			return;
+		}
+	}
+
+	public void reset() {
+		mSlider = Slider.LEFT;
+		mLayoutParams.setMargins(0, 0, 0, 0);
+	}
+
+	class FlingRunnable implements Runnable {
+		protected int step = 10;
+		protected boolean isMoveLeft = true;
+		protected float distance;
+
+		public FlingRunnable() {
+
+		}
+
+		public void setMoveLeft(boolean isMoveLeft) {
+			this.isMoveLeft = isMoveLeft;
+		}
+
+		private void startCommon() {
+			removeCallbacks(this);
+		}
+
+		@Override
+		public void run() {
+			if (isMoveLeft) {
+				distance -= step;
+				if ((distance - sliderWidth / 2) < 0) {
+					mLayoutParams.setMargins(0, 0, 0, 0);
+					tag_ImageView.setLayoutParams(mLayoutParams);
+				} else {
+					mLayoutParams.setMargins(
+							(int) (distance - sliderWidth / 2), 0, 0, 0);
+					tag_ImageView.setLayoutParams(mLayoutParams);
+					post(this);
+				}
+			} else {
+				distance += step;
+				if ((distance - sliderWidth / 2) > maxSliderWidth) {
+					mLayoutParams.setMargins(maxSliderWidth, 0, 0, 0);
+					tag_ImageView.setLayoutParams(mLayoutParams);
+				} else {
+					mLayoutParams.setMargins(
+							(int) (distance - sliderWidth / 2), 0, 0, 0);
+					tag_ImageView.setLayoutParams(mLayoutParams);
+					post(this);
+				}
+			}
+		}
+
+		public void startUsingDistance(float distance) {
+			this.distance = distance;
+			startCommon();
+			post(this);
+		}
+
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		return false;
+	}
+
+	public void setOnSliderChangeListener(OnSliderChangeListener l) {
+		this.mOnSliderChangeListener = l;
+	}
+
+	public interface OnSliderChangeListener {
+		public void onChange(Slider slider);
+	}
+
+}
