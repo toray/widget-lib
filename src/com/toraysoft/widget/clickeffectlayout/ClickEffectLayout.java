25f9a3b74364f7a057802d02e15badfd25283163
jclang <liang@toraysoft.com>
2015-03-16 16:17:21 +0800

    add clickeffectlayout

---
+++ src/com/toraysoft/widget/clickeffectlayout/ClickEffectLayout.java
@@ -0,0 +1,63 @@
+package com.toraysoft.widget.clickeffectlayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class ClickEffectLayout extends LinearLayout {
+
+	public ClickEffectLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public ClickEffectLayout(Context context) {
+		super(context);
+		init();
+	}
+
+	void init() {
+		setOrientation(LinearLayout.VERTICAL);
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		requestDisallowInterceptTouchEvent(true);
+		if (ev.getAction() == MotionEvent.ACTION_DOWN
+				|| ev.getAction() == MotionEvent.ACTION_MOVE) {
+			setAlpha(0.5f);
+		} else {
+			setAlpha(1);
+		}
+		return super.dispatchTouchEvent(ev);
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN
+				|| event.getAction() == MotionEvent.ACTION_MOVE) {
+			setAlpha(0.5f);
+		} else {
+			setAlpha(1);
+		}
+		return super.onTouchEvent(event);
+	}
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		if (ev.getAction() == MotionEvent.ACTION_DOWN
+				|| ev.getAction() == MotionEvent.ACTION_MOVE) {
+			setAlpha(0.5f);
+		} else {
+			setAlpha(1);
+		}
+		return super.onInterceptTouchEvent(ev);
+	}
+
+}
