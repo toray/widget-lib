4e93337d4b9a36bbd3e7f66b35a7c52c0a4e61ac
jclang <420115045@qq.com>
2015-04-30 12:18:02 +0800

    fix imageviewpager

--- src/com/toraysoft/widget/imageviewpager/ImageViewPager.java
+++ src/com/toraysoft/widget/imageviewpager/ImageViewPager.java
@@ -91,6 +91,9 @@ public class ImageViewPager extends RelativeLayout {
 
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
+			if(views.get(position).getParent() != null) {
+				((ViewGroup)views.get(position).getParent()).removeView(views.get(position));	
+			}
 			((ViewPager) container).addView(views.get(position));
 			return views.get(position);
 		}
